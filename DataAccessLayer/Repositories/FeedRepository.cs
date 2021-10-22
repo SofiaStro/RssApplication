1ef1fdf8780a1681c3b7bb76e21cab4d9e4d69a3
Sofia Strömberg <83826297+SofiaStro@users.noreply.github.com>
2021-10-22 09:59:23 +0200

    Skapat Repository klasser/interface

---
+++ DataAccessLayer/Repositories/FeedRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Classes;
+
+namespace DataAccessLayer.Repositories
+{
+    public class FeedRepository : IFeedRepository<Feed>
+    {
+        public void Create(Feed entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Feed> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Feed GetByCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Feed GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetIndex(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int index, Feed entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/IFeedRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Classes;
+
+namespace DataAccessLayer.Repositories
+{
+    public interface IFeedRepository<T> : IRepository<T> where T : Feed
+    {
+        T GetByName(string name);
+
+        T GetByCategory(Category category);
+
+        int GetIndex(string name);
+    }
+}

3bdd7f0b4c1b633fbdd1a02f50bc311f03f7657a
Moa Hedlund <83755862+moahedlund@users.noreply.github.com>
2021-10-22 11:45:29 +0200

    påbörjat BL (error)

---
+++ BusinessLayer/FeedService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using DataAccessLayer.Repositories;
+using Models.Classes;
+using Models;
+
+namespace BusinessLayer
+{
+    public class FeedService
+    {
+        IFeedRepository<Feed> feedRepository;
+
+        public FeedService()
+        {
+            feedRepository = new FeedRepository();
+        }
+        public void CreateFeed(string name, int numberOfEpisodes, int timeInterval, 
+                            Category category, List<Episode> listOfEpisodes )
+        {
+            listOfEpisodes = Class1.GetFeed();
+
+            Feed newFeed = null;
+            //if (objectType.Equals("Podcast"))
+            //{
+                newFeed = new Podcast(name, numberOfEpisodes, timeInterval, category, listOfEpisodes);
+            //}
+            ////if (objectType.Equals("Nyhet"))
+            ////{
+            //    newFeed = new News(name, numberOfEpisodes, timeInterval, category, listOfEpisodes);
+            //}
+            feedRepository.Create(newFeed);
+        }
+    }
+}
